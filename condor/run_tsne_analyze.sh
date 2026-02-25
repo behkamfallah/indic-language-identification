f75b06a59fea5cfe5df16abbc66dbd82fc908b9b
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2026-02-25 19:26:08 +0100

    queue models, add matplotlib install

--- condor/run_tsne_analyze.sh
+++ condor/run_tsne_analyze.sh
@@ -5,6 +5,7 @@ MY_ROOT="fati/indic-language-identification"
 PROJECT_ROOT="${HOME}/${MY_ROOT}"
 cd "${PROJECT_ROOT}"
 
+pip install matplotlib --quiet
 # python3 $1 analyze_tsne.py \
 #   --config task1_baseline_mms300m.yaml \
 python3 $1 --config configs/$2
