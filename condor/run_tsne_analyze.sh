705d5b6a5b1daa814b72fb416663c31ff47bf40b
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2026-02-25 17:41:52 +0100

    .sh mistake

--- condor/run_tsne_analyze.sh
+++ condor/run_tsne_analyze.sh
@@ -7,8 +7,4 @@ cd "${PROJECT_ROOT}"
 
 # python3 $1 analyze_tsne.py \
 #   --config task1_baseline_mms300m.yaml \
-python3 $1 \
-    --config configs/$2 \
-  # --model_dir ./models/<YOUR_BASELINE_RUN_ID_FOLDER> \
-  # --split eval \
-  # --out_dir ./outputs/tsne_baseline
+python3 $1 --config configs/$2
