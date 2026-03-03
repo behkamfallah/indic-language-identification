ac74751c8edb88b24890d6254e13223415670478
fatemeh-moghaddam <famo00001@uni-saarland.de>
2026-03-03 22:52:01 +0100

    tnse cluster files

---
+++ condor/run_tsne_analyze.sh
@@ -0,0 +1,11 @@
+pip install --upgrade wandb --quiet
+export WANDB_API_KEY=$(cat wandb_api_key)
+
+MY_ROOT="indic-language-identification"
+PROJECT_ROOT="${HOME}/${MY_ROOT}"
+cd "${PROJECT_ROOT}"
+
+pip install matplotlib --quiet
+# python3 $1 analyze_tsne.py \
+#   --config task1_baseline_mms300m.yaml \
+python3 $1 --config configs/$2 --split "train"
