717ac408b2bba91b01ade842acd59ac1720c4145
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2026-03-04 00:33:50 +0100

    the best model

--- condor/run_tsne_analyze.sh
+++ condor/run_tsne_analyze.sh
@@ -1,4 +1,4 @@
-MY_ROOT="indic-language-identification"
+MY_ROOT="fati/indic-language-identification"
 PROJECT_ROOT="${HOME}/${MY_ROOT}"
 cd "${PROJECT_ROOT}"
 
@@ -8,4 +8,4 @@ export WANDB_API_KEY=$(cat .wandb_api_key)
 pip install matplotlib --quiet
 # python3 $1 analyze_tsne.py \
 #   --config task1_baseline_mms300m.yaml \
-python3 $1 --config configs/$2 --split "train"
+python3 $1 --config configs/$2 --split "train" --model_dir "${HOME}/saj/indic-language-identification/models/task1_baseline_mms300m_20260226_231109"
